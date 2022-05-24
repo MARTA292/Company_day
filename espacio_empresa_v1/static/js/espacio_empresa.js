27ad033a30a1162ada05189667e6df08751407c5
Marta <56959222+MARTA292@users.noreply.github.com>
2022-05-24 13:56:10 +0200

    Update espacio empresa

---
+++ espacio_empresa_v1/static/js/espacio_empresa.js
@@ -0,0 +1,20 @@
+function showActividades(){
+    window.location.href = "espacio_empresa.html";
+}
+
+function showFair(){
+    window.location.href = "company_fair.html";    
+}
+
+function showPitches(){
+    window.location.href = "pitches.html"; 
+}
+
+function showMeetings(){
+    window.location.href = "meeting.html"; 
+}
+
+function showTalks(){
+    console.log("talks");
+    window.location.href = "talks.html"; 
+}
